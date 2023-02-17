e589dafbccb0d852586534d58ab76872b9580b78
Gabriela Martinez <gabrielamar26@yahoo.es>
2023-02-16 19:39:29 -0500

    Guia 9 Manos a la Obra - Ejercicio 2 - Comentarios

--- src/g9_guia/Manos_Obra_2.java
+++ src/g9_guia/Manos_Obra_2.java
@@ -21,18 +21,33 @@ public class Manos_Obra_2 {
     public static void main(String[] args) {
         // TODO code application logic here
         
+        //creacion clase jugador
         Jugador j1 = new Jugador();
         j1.setNombre("Gaby");
         j1.setApellido("Martinez");
         j1.setPosicion("delantero");
         j1.setNumero(10);
         
-        ArrayList<Jugador> jugadores = new ArrayList();
-        jugadores.add(j1);
+        Jugador j2 = new Jugador();
+        j2.setNombre("Julia");
+        j2.setApellido("Jimenez");
+        j2.setPosicion("portero");
+        j2.setNumero(7);
         
+        //creacion lista de jugadores
+        ArrayList<Jugador> jugadores = new ArrayList();
+        
+        //añadir el jugador creado a la lista
+        jugadores.add(j1);
+        jugadores.add(j2);
+        
+        //creacion clase equipo
         Equipo e1 = new Equipo();
+        
+        //a la clase equipo se le envía la lista con de objetos jugadores creados
         e1.setJugadores(jugadores);
         
+        //imprimir los objetos jugadores en la lista
         for (Jugador aux : jugadores) {
             System.out.println(aux.toString());
         }
